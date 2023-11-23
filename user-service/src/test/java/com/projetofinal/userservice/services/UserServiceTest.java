e7c0e9c916d94a39580b64167b1077f48fbd6967
Isabela Baseggio <isabela.baseggio7@gmail.com>
2023-11-23 00:47:43 -0300

    developer: UserServiceTest testAddUser passou

--- user-service/src/test/java/com/projetofinal/userservice/services/UserServiceTest.java
+++ user-service/src/test/java/com/projetofinal/userservice/services/UserServiceTest.java
@@ -9,6 +9,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.testng.AssertJUnit.assertEquals;
@@ -23,19 +26,28 @@ public class UserServiceTest {
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
     public void testAddUser() {
         RequestUser requestUser = new RequestUser("Ana", 20, "ana@email.com", "adc123");
-        UserModel user = new UserModel(requestUser);
+        UserModel user = new UserModel(requestUser); // @user16546
+        String email = "ana@email.com";
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+
         when(userRepository.save(user)).thenReturn(user);
 
-        UserModel resultado = userService.addUser(requestUser);
+        UserModel resultado = userService.addUser(requestUser); // @user475452
 
-        verify(userRepository).save(user);
+        verify(userRepository).save(resultado);
 
-        assertEquals(user, resultado);
+        // assertEquals(user, resultado); // @user16546 , // @user475452 -> erro pois são instâncias diferentes
+
+        assertEquals(user.getNome_usuario(), resultado.getNome_usuario());
+        assertEquals(user.getIdade(), resultado.getIdade());
+        assertEquals(user.getEmail(), resultado.getEmail());
+        assertEquals(user.getSenha(), resultado.getSenha());
     }
 }
