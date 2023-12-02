08bc364e849f5330e076b86f9a05ebc4ddf9d491
Isabela Baseggio <isabela.baseggio7@gmail.com>
2023-12-02 01:53:46 -0300

    :bug: :recycle: fix & refactor: removendo WebClient

--- zelda-service/src/main/java/com/projetofinal/zeldaservice/ZeldaServiceApplication.java
+++ zelda-service/src/main/java/com/projetofinal/zeldaservice/ZeldaServiceApplication.java
@@ -10,13 +10,6 @@ import org.springframework.http.HttpHeaders;
 
 @SpringBootApplication
 public class ZeldaServiceApplication {
-    @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl("https://zelda.fanapis.com/api")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(ZeldaServiceApplication.class, args);
