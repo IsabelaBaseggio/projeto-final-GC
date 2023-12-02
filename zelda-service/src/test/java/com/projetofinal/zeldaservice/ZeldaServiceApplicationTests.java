ad4fa9cfd643c4627dbe7e4b2561c93938933c4a
Isabela Baseggio <isabela.baseggio7@gmail.com>
2023-12-02 01:54:53 -0300

    :bug: :recycle: :white_check_mark:	fix & refactor & test: teste do método listGames usando mockando RestTemplate

--- zelda-service/src/test/java/com/projetofinal/zeldaservice/ZeldaServiceApplicationTests.java
+++ zelda-service/src/test/java/com/projetofinal/zeldaservice/ZeldaServiceApplicationTests.java
@@ -2,6 +2,10 @@ package com.projetofinal.zeldaservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootTest
 class ZeldaServiceApplicationTests {
